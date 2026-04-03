777f604fcc1a1b7e7ed64a3c9d7af6a82ff1f994
mdossantoscarrera-ship-it <mdossantoscarrera@gmail.com>
2026-04-03 15:49:10 -0300

    Add files via upload

---
+++ DataService.cs
@@ -0,0 +1,54 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "6135072afe7f6cec1537d5cb08a5a1a2";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}&lang=pt_br";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime sunrise = DateTimeOffset
+                        .FromUnixTimeSeconds((long)rascunho["sys"]["sunrise"])
+                        .LocalDateTime;
+
+                    DateTime sunset = DateTimeOffset
+                        .FromUnixTimeSeconds((long)rascunho["sys"]["sunset"])
+                        .LocalDateTime;
+
+                    t = new Tempo()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString("HH:mm"),
+                        sunset = sunset.ToString("HH:mm"),
+                    };
+                }
+            }
+
+            return t;
+        }
+    }
+}
---
+++ Tempo.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiAppTempoAgora.Models
+{
+    public class Tempo
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string description { get; set; }
+        public string main { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public double speed { get; set; }
+        public int visibility { get; set; }
+        public string sunrise { get; set; }
+        public string sunset { get; set; }
+    }
+}
